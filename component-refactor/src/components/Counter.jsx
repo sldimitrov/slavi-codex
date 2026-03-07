c0236cf734a8ff01b03ab9ff6e456c3a66856660
Slavi FE <slavidimitrov54@gmail.com>
2026-03-07 20:32:56 +0200

    all slavi-codex tutorials

--- component-refactor/src/components/Counter.jsx
+++ component-refactor/src/components/Counter.jsx
@@ -1,44 +1,46 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0,
-      windowWidth: window.innerWidth
-    };
-  }
+const Counter = () => {
+  const [ count, setCount ] = useState(0);
+  const [ windowWidth, setWindowWidth ] = useState(0)
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     count: 0,
+  //   };
+  // }
 
   // Effect 1: Syncing state with the document title
   // Effect 2: Setting up an event listener
-  componentDidMount() {
-    document.title = `Count: ${this.state.count}`;
-    window.addEventListener('resize', this.handleResize);
-  }
+  // componentDidMount() {
+  //   document.title = `Count: ${this.state.count}`;
+  //   window.addEventListener('resize', this.handleResize);
+  // }
+  //
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.count !== this.state.count) {
+  //     document.title = `Count: ${this.state.count}`;
+  //   }
+  // }
+  //
+  // componentWillUnmount() {
+  //   window.removeEventListener('resize', this.handleResize);
+  // }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.count !== this.state.count) {
-      document.title = `Count: ${this.state.count}`;
-    }
-  }
+  // const handleResize = () => {
+  //   setWindowWidth(window.innerWidth);
+  // };
 
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.handleResize);
-  }
-
-  handleResize = () => {
-    this.setState({ windowWidth: window.innerWidth });
+  const handleIncrement = () => {
+    setCount((prev) => prev + 1);
   };
 
-  increment = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  render() {
+  {
     return (
       <div style={{ padding: '20px' }}>
-        <h1>Count: {this.state.count}</h1>
-        <button onClick={this.increment}>Increment</button>
+        <h1>Count: {count}</h1>
+        <button onClick={() => handleIncrement()}>Increment</button>
         <hr />
         <p>The window width is: **{this.state.windowWidth}px**</p>
       </div>
