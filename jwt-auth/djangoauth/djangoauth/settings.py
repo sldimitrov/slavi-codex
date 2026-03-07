4a8aa7388d1e236b60eab4ef2ddaf6990cfed725
Slavi FE <slavidimitrov54@gmail.com>
2026-03-07 20:30:48 +0200

    auth - finalize code, add cors to BE and logic to FE

--- jwt-auth/djangoauth/djangoauth/settings.py
+++ jwt-auth/djangoauth/djangoauth/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     'rest_framework',
     'rest_framework_simplejwt',
     'django.contrib.admin',
@@ -48,6 +49,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000", # Common for Create React App
+    "http://localhost:5173", # Common for Vite
+    "http://localhost:5174", # Common for Vite
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5173",
 ]
 
 ROOT_URLCONF = 'djangoauth.urls'
--- jwt-auth/react-jwt-auth/src/App.jsx
+++ jwt-auth/react-jwt-auth/src/App.jsx
@@ -1,34 +1,121 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from "react";
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [authError, setAuthError] = useState(null)
+  const [isSecretShown, setIsSecretShown] = useState(false)
+  const [secretMessage, setSecretMessage] = useState('')
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const payload = { username, password };
+
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/token/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Login failed: ${response.status}`);
+      }
+
+      const result = await response.json();
+      console.log("Success! Here are your tokens:", result);
+
+      // result.access is "Key"
+      // result.refresh is "Backup Key"
+
+      localStorage.setItem("access_token", result.access)
+    } catch (error) {
+      console.error("Oops!", error.message);
+      setAuthError(error.message);
+    } finally {
+      setIsSecretShown(true)
+      setUsername('')
+      setPassword('')
+    }
+  }
+
+  async function fetchSecretData() {
+    const token = localStorage.getItem("access_token");
+
+    console.log("token", token)
+
+    if (!token) {
+      setIsSecretShown(false);
+      setAuthError("Authentication is required to access hidden resources!")
+      return;
+    }
+
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/test/auth/', {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        }
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setSecretMessage(data.message + " - Secret: " + data.secret_code);
+      } else {
+        setSecretMessage("Access Denied! Token might be expired.");
+      }
+
+      setIsSecretShown(true);
+    } catch(error) {
+      console.error("Oops!", error.message);
+    }
+  }
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>
+          Form using JWT for Authentication
+        </h2>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div>
+            <label htmlFor="username">Username</label><br/>
+            <input
+              id="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="Password">Password</label><br/>
+            <input
+              id="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <span className="error">{authError}</span><br/>
+          <button type="submit">Submit</button>
+        </form>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <h2>Secret Resources Library</h2>
+        <section>
+          <button onClick={fetchSecretData}>Show hidden resources</button>
+          <br/>
+          { isSecretShown && <span className="secret">{secretMessage}</span>}
+        </section >
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
